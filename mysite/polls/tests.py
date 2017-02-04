8675ce5e30c248858afc7e503a88b895da3f7276
adam huang <huangyu@huangyudeMac-mini.local>
2017-02-04 10:50:26 +0800

    添加更综合的测试，判断当前和过去的时间

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -7,7 +7,8 @@ from django.utils import timezone
 from .models import Question
 
 # Create your tests here.
-#编写测试
+#1.编写测试
+
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -19,3 +20,25 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+    #2.编写更加综合的测试
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for question whose
+        pub_date is in older than 1 day.
+        """
+
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for question whose
+        pub_date is within the last day.
+        """
+
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
