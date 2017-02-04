7c47e3121b08ab7f84bfdc6668ef077cf4fb5612
adam huang <huangyu@huangyudeMac-mini.local>
2017-02-04 10:35:51 +0800

    添加测试并且修复未来提交时间问题

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,21 @@
+#-*-coding: utf-8 -*-
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+#编写测试
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
