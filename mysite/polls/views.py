12bc80c2b25d8d31ecf1fc8ff11aecfff743ea9e
adam huang <huangyu@huangyudeMac-mini.local>
2017-02-04 17:18:25 +0800

    为改进的新视图添加测试

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
+#-*-coding: utf-8 -*-
 
-
-
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -13,9 +13,14 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
+#改进视图
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions (not including those set to be
+        published in the future)
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
