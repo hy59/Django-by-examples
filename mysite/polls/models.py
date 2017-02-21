5301f2b60c066118295c4c0aff94395ece6590e3
adam huang <937162905@qq.com>
2017-02-21 20:25:18 +0800

    Add test (#3)
    * add results.html,change the result()
    
    * 使用通用视图
    
    * 添加测试并且修复未来提交时间问题
    
    * 添加更综合的测试，判断当前和过去的时间
    
    * 为改进的新视图添加测试
    
    * fix#2 添加测试：未来的问题不能访问

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,4 @@
-
+#-*-coding: utf-8-*-
 
 
 import datetime
@@ -12,10 +12,9 @@ class Question(models.Model):
     def __unicode__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-        was_published_recently.admin_order_field = 'pub_date'
-        was_published_recently.boolean = True
-        was_published_recently.short_description = 'Published recently?'
+        #修复未来发布时间错误
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
+#-*-coding: utf-8 -*-
 
-
-
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -13,13 +13,35 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
+<<<<<<< HEAD
+#改进视图
+    def get_queryset(self):
+        """Return the last five published questions (not including those set to be
+        published in the future)
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+=======
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
+>>>>>>> master
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+<<<<<<< HEAD
+    """
+    添加约束
+    """
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+=======
+>>>>>>> master
 
 class ResultsView(generic.DetailView):
     model = Question
